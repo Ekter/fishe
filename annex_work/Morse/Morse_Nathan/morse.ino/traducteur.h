dc1723c0c247db8ab8e8b5f47f0eb45155634836
Nath <nathan.dmnd45@gmail.com>
2022-11-29 18:07:29 +0100

    updated

--- annex_work/Morse/Morse_Nathan/morse.ino/traducteur.h
+++ annex_work/Morse/Morse_Nathan/morse.ino/traducteur.h
@@ -4,15 +4,14 @@ class traducteur {
   public :
     traducteur();
     //fonctions traduire :
-    void traduitDelay(String mot);    // via delay
     void traduitMillis(String mot);   // via millis
   private :
     const char court[2] = "c";          //temps court
     unsigned long currentTime = 0;      //permet utilisation de millis
     unsigned long previousTime = 0;
     char charLettre;                    // transformer une lettre d'un tableau en char
-    void allumerLedDelay(int duree);
     bool allumerLedMillis(int duree, bool ledState);
+    bool attendreMillis(int duree);
     String TabTraductlettres[28][2] = { //on met dans un tableau a double entre la lettre de l'alphabet (colonne 1) et sa traduction en morse (colonne 2)
       {"a", "cl"},
       {"b", "lccc"},
@@ -41,6 +40,16 @@ class traducteur {
       {"y", "lcll"},
       {"z", "llcc"},
       {" ", ""},
+      {"0", "lllll"},
+      {"1", "cllll"},
+      {"2", "cclll"},
+      {"3", "cccll"},
+      {"4", "ccccl"},
+      {"5", "ccccc"},
+      {"6", "lcccc"},
+      {"7", "llccc"},
+      {"8", "lllcc"},
+      {"9", "llllc"},
       {"", ""},    //cas pour verifier si c'est une lettre
     };
 };
