fb59da28047ae79570ec4766fffa2a1e021c2268
Ekter <ektermoi1@gmail.com>
2022-12-17 17:24:11 +0100

    refactor : refactor morse main.ino

--- annex_work/Morse/Morse_Nino/main.ino
+++ annex_work/Morse/Morse_Nino/main.ino
@@ -1,15 +1,19 @@
 #include "Translator.h"
 
 Translator trad = Translator();
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial.available());
+  while (!Serial.available())
+    ;
   Serial.println("la constante de plik est 37!");
   trad.translateword("la constante de plik est 37!"); // the ! is not supported, it can be used as a test
 }
 
-void loop() {
-  if (Serial.available()) {
+void loop()
+{
+  if (Serial.available())
+  {
     char letter = Serial.read();
     trad.translate(letter);
   }
