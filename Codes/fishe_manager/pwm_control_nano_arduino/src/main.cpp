793cf675479c5970d75a8a34cfba71245e20d76e
Ekter <ektermoi1@gmail.com>
2023-03-25 17:06:55 +0100

    feat : communication code

--- Codes/fishe_manager/pwm_control_nano_arduino/src/main.cpp
+++ Codes/fishe_manager/pwm_control_nano_arduino/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-
+Servo servo;
 void setup()
 {
   Serial.begin(119200);
@@ -10,6 +11,28 @@ void loop()
 {
   if (Serial.available())
   {
-    Serial.println("Hello Jetson!");
+    // Serial.println("Hello Jetson!");
+    uint8_t a = Serial.read();
+    Serial.print("Received: '");
+    Serial.print(a);
+    Serial.println("'");
+    if (a>=181 && a<=200)
+    {
+      servo.attach(a-181);
+      Serial.print("Servo attached to pin");
+      Serial.println(a-181);
+    }
+    if (a == 201)
+    {
+      servo.detach();
+      Serial.println("Servo detached");
+    }
+    if (a <= 180)
+    {
+      // uint16_t b = a-90;
+      servo.write(a);
+      Serial.print("Servo set to ");
+      Serial.println(a);
+    }
   }
 }
