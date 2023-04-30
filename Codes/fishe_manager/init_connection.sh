fc50c4db487557b066c8d7ea6727bc2d618b2bc3
Ekter <ektermoi1@gmail.com>
2023-04-30 22:42:39 +0200

    feat : edit interface name in init_connection

--- Codes/fishe_manager/init_connection.sh
+++ Codes/fishe_manager/init_connection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo nmcli con add type wifi ifname r8188eu con-name Hotspot autoconnect yes ssid Hotspot;
+sudo nmcli con add type wifi ifname wlan0 con-name Hotspot autoconnect yes ssid Hotspot;
 sudo nmcli con modify Hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared;
 sudo nmcli con modify Hotspot wifi-sec.key-mgmt wpa-psk;
 sudo nmcli con modify Hotspot wifi-sec.psk "Plik est grand";
