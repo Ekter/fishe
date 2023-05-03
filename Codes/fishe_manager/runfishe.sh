f2287c198001ca4a46197e8e367e56e7834cc491
Ekter <ektermoi1@gmail.com>
2023-05-03 21:39:06 +0200

    feat : removed hotspot automatic enable

--- Codes/fishe_manager/runfishe.sh
+++ Codes/fishe_manager/runfishe.sh
@@ -3,7 +3,7 @@
 
 source venv/bin/activate;
 sudo -H pip3 install -r requirements.txt;
-UUID=$(grep uuid /etc/NetworkManager/system-connections/Hotspot | cut -d= -f2);
-nmcli con up uuid $UUID;
+# UUID=$(grep uuid /etc/NetworkManager/system-connections/Hotspot | cut -d= -f2);
+# nmcli con up uuid $UUID;
 sudo venv/bin/python3 fishe_server/manage.py runserver 0.0.0.0:80 & 
 sudo /usr/bin/python3 manager.py && fg
